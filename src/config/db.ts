88b06ecb30cb243563cf3795a2341666c9c14566
Bryant <bryanttejeda628f@hotmail.com>
2025-06-11 17:03:43 -0400

    Creando el Modelo de Presupuestos

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,6 +4,8 @@ import colors from 'colors'
 dotenv.config()
 
 const db = new Sequelize( process.env.DATABASE_URL, {
+    models: [__dirname + '/../models/**/*'],
+    logging: false,
     dialectOptions: {
         ssl: {
             require: false
---
+++ src/models/Budget.ts
@@ -0,0 +1,21 @@
+import { Table, Column, DataType, HasMany, BelongsTo, ForeignKey, Model } from 'sequelize-typescript'
+
+//Decorator
+@Table({
+    tableName: 'budgets'
+})
+
+class Budget extends Model {
+
+    @Column({
+        type: DataType.STRING(100)
+    })
+    name: string
+
+    @Column({
+        type: DataType.DECIMAL
+    })
+    amount: number
+}
+
+export default Budget
