3f512d66f2acbbac58cd06331b152bb6b86b09dd
Bryant <bryanttejeda628f@hotmail.com>
2025-06-12 15:49:59 -0400

    Creando Middleware para validar la creación de Presupuestos

--- src/middleware/budget.ts
+++ src/middleware/budget.ts
@@ -1,5 +1,5 @@
 import type { Request, Response, NextFunction } from "express";
-import { param } from "express-validator";
+import { body, param, validationResult } from "express-validator";
 import { handleInputsErrors } from "./validation";
 import Budget from "../models/Budget";
 
@@ -12,28 +12,41 @@ declare global {
 }
 export const validateBudgetId = async (req: Request, res: Response, next: NextFunction) => {
     await param('budgetId')
-            .isInt().withMessage('ID no valido')
-            .custom(value => value > 0).withMessage('ID no valido')
-            .run(req),
-    handleInputsErrors,
+        .isInt().withMessage('ID no valido')
+        .custom(value => value > 0).withMessage('ID no valido')
+        .run(req)
     next()
 }
 
 export const validateBudgetExists = async (req: Request, res: Response, next: NextFunction) => {
     try {
-            const { budgetId } = req.params
-            const budget = await Budget.findByPk(budgetId)
+        const { budgetId } = req.params
+        const budget = await Budget.findByPk(budgetId)
 
-            if (!budget) {
-                const error = new Error('Presupuesto no encontrado')
-                res.status(404).json({ error: error.message })
-                return
-            }
-            req.budget = budget
-
-            next()
-        } catch (error) {
-            // console.log(error);
-            res.status(500).json({ error: "Hubo un error" })
+        if (!budget) {
+            const error = new Error('Presupuesto no encontrado')
+            res.status(404).json({ error: error.message })
+            return
         }
+        req.budget = budget
+
+        next()
+    } catch (error) {
+        // console.log(error);
+        res.status(500).json({ error: "Hubo un error" })
+    }
+}
+
+export const validateBudgetInput = async (req: Request, res: Response, next: NextFunction) => {
+
+    await body('name')
+        .notEmpty().withMessage("El nombre del presupuesto no puede ir vacio")
+        .run(req)
+    await body('amount')
+        .notEmpty().withMessage("La cantidad del presupuesto no puede ir vacio")
+        .isNumeric().withMessage('Cantidad no valida')
+        .custom(value => value > 0).withMessage("El presupuesto debe ser mayor a 0")
+        .run(req)
+    next()
+    
 }
--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 import { BudgetController } from '../controllers/BudgetController'
 import { body, param, validationResult } from 'express-validator'
 import { handleInputsErrors } from '../middleware/validation'
-import { validateBudgetExists, validateBudgetId } from '../middleware/budget'
+import { validateBudgetExists, validateBudgetId, validateBudgetInput } from '../middleware/budget'
 const router = Router()
 
 router.param('budgetId', validateBudgetId)
@@ -11,20 +11,22 @@ router.param('budgetId', validateBudgetExists)
 router.get('/', BudgetController.getAll)
 
 router.post('/',
-    body('name')
-        .notEmpty().withMessage("El nombre del presupuesto no puede ir vacio"),
-    body('amount')
-        .notEmpty().withMessage("La cantidad del presupuesto no puede ir vacio")
-        .isNumeric().withMessage('Cantidad no valida')
-        .custom(value => value > 0).withMessage("El presupuesto debe ser mayor a 0"),
+    validateBudgetInput,
     handleInputsErrors,
     BudgetController.create
 )
 
-router.get('/:budgetId', BudgetController.getById)
+router.get('/:budgetId', 
+    handleInputsErrors,
+    BudgetController.getById)
 
-router.put('/:budgetId', BudgetController.update)
+router.put('/:budgetId', 
+    validateBudgetInput,
+    handleInputsErrors,
+    BudgetController.update)
 
-router.delete('/:budgetId', BudgetController.deleteById)
+router.delete('/:budgetId', 
+    handleInputsErrors,
+    BudgetController.deleteById)
 
 export default router
