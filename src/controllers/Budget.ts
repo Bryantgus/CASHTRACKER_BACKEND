9df2f181bb0d214227b3a122cefe5deb9063df56
Bryant <bryanttejeda628f@hotmail.com>
2025-06-12 11:50:43 -0400

    Agregando todas las Rutas y Métodos de Controlador

--- src/controllers/Budget.ts
+++ src/controllers/Budget.ts
@@ -9,4 +9,16 @@ export class BudgetController {
     static create = async (req: Request, res: Response) => {
         console.log("Desde create");
     }
+
+    static getById = async (req: Request, res: Response) => {
+        console.log("Desde getById");
+    }
+
+    static update = async (req: Request, res: Response) => {
+        console.log("Desde update");
+    }
+
+    static deleteById = async (req: Request, res: Response) => {
+        console.log("Desde deleteById");
+    }
 }
