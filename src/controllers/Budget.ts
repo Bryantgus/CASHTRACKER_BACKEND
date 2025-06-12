7949687f207a2efe2b8ce195f5fc07777cf28b18
Bryant <bryanttejeda628f@hotmail.com>
2025-06-12 11:56:54 -0400

    Creación de Presupuestos

--- src/controllers/Budget.ts
+++ src/controllers/Budget.ts
@@ -1,4 +1,5 @@
 import { Request, Response  } from "express"
+import Budget from "../models/Budget";
 
 export class BudgetController {
 
@@ -7,7 +8,15 @@ export class BudgetController {
     }
 
     static create = async (req: Request, res: Response) => {
-        console.log("Desde create");
+        try {
+            const budget = new Budget(req.body)
+            await budget.save()
+            res.status(201).json("Presupuesto creado correctamente")
+        } catch (error) {
+            // console.log(error);
+            res.status(500).json({ error: "Hubo un error"})
+            
+        }
     }
 
     static getById = async (req: Request, res: Response) => {
