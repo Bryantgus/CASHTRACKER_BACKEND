b7ec229def543754436d07126921438c549003ad
Bryant Tejeda <bryanttejeda628f@hotmail.com>
2025-06-15 16:32:58 -0400

    Validación en el Registro de Usuarios

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,9 +1,19 @@
 import { Router } from "express"
 import AuthController from "../controllers/authController"
 
+import { body } from 'express-validator'
+import { handleInputsErrors } from "../middleware/validation"
+
 
 const router = Router()
 
-router.post('/createAccount', AuthController.createAccount)
+router.post('/createAccount',
+    body('name').notEmpty().withMessage('El nombre no puede ir vacio'),
+    body('password')
+        .isLength({min:8}).withMessage('El password es muy corto, minimo 8 caracteres'),
+    body('email')
+        .isEmail().withMessage('Email no valido'),
+    handleInputsErrors,
+    AuthController.createAccount)
 
 export default router
