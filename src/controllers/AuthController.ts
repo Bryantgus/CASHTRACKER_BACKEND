d5e759c29531c6a4445d89c880d637d9236a1e42
Bryant Tejeda <bryanttejeda628f@hotmail.com>
2025-06-15 16:35:41 -0400

    Registrando Usuarios

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express'
+import User from '../models/User'
 
 export class AuthController {
   
     static createAccount = async (req: Request, res: Response) => {
-        res.json("Desde create account")
+        try {
+            const user = new User(req.body)
+            await user.save()
+            res.json('Cuenta creada correctamente')
+        } catch (error) {
+            // console.log(error);
+            res.status(500).json({error: 'Hubo un error'})
+            
+        }
     }
 }
 
