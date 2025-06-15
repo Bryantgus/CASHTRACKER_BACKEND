c43167c2c4505486f8fec494536d67de2abb58fc
Bryant Tejeda <bryanttejeda628f@hotmail.com>
2025-06-15 16:54:31 -0400

    Generando un Token para confirmar la cuenta

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import User from '../models/User'
 import { hashPassword } from '../utils/auth'
+import { generateToken } from '../utils/token'
 
 export class AuthController {
   
@@ -19,6 +20,7 @@ export class AuthController {
             const { password } = req.body
             const user = new User(req.body)
             user.password = await hashPassword(password)
+            user.token = generateToken()
             await user.save()
             res.json('Cuenta creada correctamente')
             return
