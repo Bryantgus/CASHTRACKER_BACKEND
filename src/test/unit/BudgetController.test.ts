37ae90038857f307d117de9f2917ccf59f8b29dc
Bryant <bryanttejeda628f@hotmail.com>
2025-06-18 09:56:20 -0400

    Agregando un Mock al Controlador de Gastos

--- src/test/unit/BudgetController.test.ts
+++ src/test/unit/BudgetController.test.ts
@@ -1,15 +1,24 @@
 import { BudgetController } from "../../controllers/BudgetController"
 import { budgets } from "../mocks/budgets"
 import { createMocks, createRequest, createResponse} from 'node-mocks-http'
+import Budget from '../../models/Budget'
+
+jest.mock('../../models/Budget', () => ({
+    findAll: jest.fn()
+}))
+
 describe('BudgetController.getAll', () => {
     it('should retrieve 3 budgets', async () => {
         
         const req = createRequest({
             method: 'GET',
-            url: '/api/budget'
+            url: '/api/budget',
+            user: {id: 500}
         })
 
-        const res = createResponse()
+        const res = createResponse();
+
+        (Budget.findAll as jest.Mock).mockResolvedValue(budgets)
 
         await BudgetController.getAll(req, res)
     })
